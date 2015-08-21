8e25911bcb491a6c285ff45798a7b0148b177dd1
Henry Sudarma <henry.sudarma@adibrata.co.id>
2015-08-21 17:09:11 +0700

    Henry - Settin Struts XML

---
+++ src/com/adibrata/smartdealer/action/accmnt/suspend/SuspendListAction.java
@@ -0,0 +1,15 @@
+
+package com.adibrata.smartdealer.action.accmnt.suspend;
+
+import com.adibrata.smartdealer.action.BaseAction;
+import com.opensymphony.xwork2.Preparable;
+
+public class SuspendListAction extends BaseAction implements Preparable
+	{
+
+		public SuspendListAction() throws Exception
+			{
+				// TODO Auto-generated constructor stub
+			}
+
+	}
